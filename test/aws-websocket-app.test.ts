3f24edb06cc350b5316a19695b157f30dec424a1
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-06-25 22:41:51 +0900

    Initial commit

---
+++ test/aws-websocket-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsWebsocketApp from '../lib/aws-websocket-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-websocket-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsWebsocketApp.AwsWebsocketAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
