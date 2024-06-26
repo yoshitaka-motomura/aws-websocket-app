9549507a2a6dcfbfe5975a4fcdc580db786e4a4a
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-06-27 00:09:05 +0900

    Update dependencies and enhance WebSocket setup
    This commit includes the update of several packages including 'aws-cdk-lib' to the version 2.147.1. Additionally, it establishes a new setup for WebSockets, including the integration of the 'dotenv' package for environment variable management. Changes in the DynamoDB table definition, Lambda function definition, and API Gateway configuration were introduced as part of these improvements.

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -2,12 +2,19 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ApigwAppStack } from '../lib/apigw-app-stack';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
+const certificateArn = process.env.CERTIFICATE_ARN || null
+if (!certificateArn) {
+    throw new Error('CERTIFICATE_ARN environment variable is required')
+}
 const appName = 'EnceladusService'
 const app = new cdk.App();
 
 app.node.setContext('appName', appName);
 app.node.setContext('stage', 'dev');
+app.node.setContext('certificateArn', certificateArn);
 
 new ApigwAppStack(app, `${appName}Stack`, {
     env: {
--- lib/apigw-app-stack.ts
+++ lib/apigw-app-stack.ts
@@ -6,39 +6,86 @@ import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import { Function } from 'aws-cdk-lib/aws-lambda'
 import { WebSocketLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as path from "node:path";
 
 export class ApigwAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const appName = this.node.tryGetContext('appName');
     const stage = this.node.tryGetContext('stage') || 'dev';
+    const certificate_arn = this.node.tryGetContext('certificateArn');
+
+    // dynamodb table definition
+    const connectionTable = new dynamodb.Table(this, 'ConnectionTable', {
+      tableName: 'connections',
+        partitionKey: {
+            name: 'connectionId',
+            type: dynamodb.AttributeType.STRING
+        },
+        sortKey: {
+            name: 'location_id',
+            type: dynamodb.AttributeType.STRING
+        },
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    })
+
+    connectionTable.addGlobalSecondaryIndex({
+        indexName: 'locationIndex',
+        partitionKey: {
+          name: 'location_id',
+          type: dynamodb.AttributeType.STRING
+        }
+    })
+
+    // lambda function definition
+    const lambdaHandler = new Function(this, 'ConnectFunction', {
+      runtime: cdk.aws_lambda.Runtime.PYTHON_3_12,
+      handler: 'app.lambda_handler',
+      code: cdk.aws_lambda.Code.fromAsset(path.join(__dirname, '../lambda/')),
+      functionName: `${appName}WebSocketsFunction`,
+        logGroup: new cdk.aws_logs.LogGroup(this, 'LogGroup', {
+            logGroupName: `/aws/lambda/${appName}WebSocketsFunction`,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        })
+    })
+
+    lambdaHandler.addEnvironment('TABLE_NAME', connectionTable.tableName)
+    connectionTable.grantFullAccess(lambdaHandler)
+
 
     // lambda function definition
     const webSocketApi = new apigw.WebSocketApi(this, 'Api', {
       apiName: `${appName}Api`,
       description: `API for ${appName}`,
       connectRouteOptions: {
-        // integration for connect route
+          integration: new WebSocketLambdaIntegration('connect', lambdaHandler)
       },
-
+      disconnectRouteOptions: {
+          integration: new WebSocketLambdaIntegration('disconnect', lambdaHandler)
+      },
+      defaultRouteOptions: {
+          integration: new WebSocketLambdaIntegration('default', lambdaHandler)
+      }
     })
-    new apigw.WebSocketStage(this, 'stage', {
+    const webSocketStage = new apigw.WebSocketStage(this, 'stage', {
       webSocketApi,
       stageName: stage,
       autoDeploy: true
     })
 
-    const domainName = 'example.com'
-    const certificateArn = 'certificate-arn'
+    const domainName = 'socket.cristallum.io'
+    const certificateArn = certificate_arn as string
     const certificate = acm.Certificate.fromCertificateArn(
       this,
       'Certificate',
       certificateArn
     );
 
-    const hostedZone = new route53.HostedZone(this, 'HostedZone', {
-      zoneName: 'your zone-name',
-    })
+   const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+    domainName: 'cristallum.io',
+  });
     const domain = new apigw.DomainName(this, 'Domain', {
       domainName: domainName,
       certificate: certificate
@@ -47,7 +94,7 @@ export class ApigwAppStack extends cdk.Stack {
     new apigw.ApiMapping(this, 'Mapping', {
         api: webSocketApi,
         domainName: domain,
-        stage: stage,
+        stage: webSocketStage
     })
 
     new route53.ARecord(this, 'AliasRecord', {
